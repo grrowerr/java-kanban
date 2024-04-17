e2c0f94733487dc4620ed2c3a5ab2e872a9c6ff9
grrowerr <grouw.uzkiy@yandex.ru>
2024-04-17 03:48:04 +0300

    Add new constructor and fix methods

---
+++ src/ru/yandex/javacource/fadeev/schedule/task/Epic.java
@@ -0,0 +1,36 @@
+package ru.yandex.javacource.fadeev.schedule.task;
+
+import java.util.ArrayList;
+public class Epic extends Task {
+
+    private ArrayList<Integer> subTasksIds = new ArrayList<>();
+
+    public Epic (String title, String description, int id, Status status) {
+        super(title, description, id, status);
+    }
+
+    public Epic(String title, String description) {
+        super(title, description, Status.NEW);
+    }
+
+    public ArrayList<Integer> getSubTasksIds() {
+        return subTasksIds;
+    }
+
+    public void setSubTasksIds(ArrayList<Integer> subTasksIds) {
+        this.subTasksIds = subTasksIds;
+    }
+
+    public void addSubTaskId(int subTaskId) {
+        subTasksIds.add(subTaskId);
+    }
+
+    public void removeSubTaskId(Integer subTaskId) {
+        subTasksIds.remove(subTaskId);
+    }
+
+    public void deleteSubTasksIds() {
+        subTasksIds.clear();
+    }
+
+}
